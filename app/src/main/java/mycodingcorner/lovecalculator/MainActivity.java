4c9be13b9e56a04d5f5b9a95a8f1930ef05955d9
N-Avinash <nethalaavinash94@gmail.com>
2016-02-11 01:39:42 +0530

    Initial commit

---
+++ app/src/main/java/mycodingcorner/lovecalculator/MainActivity.java
@@ -0,0 +1,74 @@
+package mycodingcorner.lovecalculator;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        Button cal = (Button) findViewById(R.id.resbtn);
+        final TextView res = (TextView) findViewById(R.id.resview);
+        final EditText name = (EditText) findViewById(R.id.nameField);
+        final EditText cname = (EditText) findViewById(R.id.crushnameField);
+        cal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Editable n = name.getText();
+                Editable cn = cname.getText();
+                String concat =  String.valueOf(n).concat(String.valueOf(cn)).toUpperCase();
+                int sum = 0;
+                for (int i = 0; i < concat.length(); i++) {
+                    char character = concat.charAt(i);
+                    int ascii = (int) character;
+                    sum += ascii;
+                }res.setText("The love between " + n + " and " + cn + " is " + sum % 100 + "%");
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
